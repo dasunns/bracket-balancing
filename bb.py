c1179b2bbc8bc1f51076f8fbddc7da32a7e13f44
dasunns <qplesoft@gmail.com>
2023-04-13 13:59:02 +0530

    added fib function

--- bb.py
+++ bb.py
@@ -1,2 +1,12 @@
 print("Hello")
 print("hello world")
+#function to print fibinnacci series
+def print_fib(x):
+    if x == 0:
+        return 0
+    elif x == 1:
+        return 1
+    else:
+        return print_fib(x-1) + print_fib(x-2)
+
+print(print_fib(3))
